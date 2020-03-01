2bffb6a2e84725e9144e8fc55705df147b167e47
Amanda Contreras <amandaben21@aol.com>
2020-03-01 03:28:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+  "This is a string! It has three sentences. Right?" 
+  self.count_sentences("!.?")
+  split.count_sentences(%r{,\s*})
+ end
+end
+ 
+ 
+ 
+ 
+ 
 end
