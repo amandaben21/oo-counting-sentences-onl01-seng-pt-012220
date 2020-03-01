8d586c5419f3db9c8c544f499b62f0cdf734354d
Amanda Contreras <amandaben21@aol.com>
2020-03-01 03:30:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -13,7 +13,7 @@ class String
   end
 
   def count_sentences
-  "This is a string! It has three sentences. Right?" 
+  puts "This is a string! It has three sentences. Right?" 
   self.count_sentences("!.?")
   split.count_sentences(%r{,\s*})
  end
